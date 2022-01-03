7819de18c2dad2b0ac617f62d80b2e9de2e5714d
programmer-shaheb <abidurrahman471@gmail.com>
2022-01-04 01:16:14 +0600

    code refactor

--- testing-app/src/App.test.js
+++ testing-app/src/App.test.js
@@ -2,138 +2,147 @@ import { render, screen } from "@testing-library/react";
 import App from "./App";
 import userEvent from "@testing-library/user-event";
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+// Helper Function
 
-// test("renders reload", () => {
-//   render(<App />);
-//   const xyz = screen.getByText(/Edit/i);
-//   expect(xyz).toBeInTheDocument();
-// });
-
-test("inputs should be in initially empty", () => {
-  render(<App />);
-  const emailInputElement = screen.getByRole("textbox");
-  const passwordInputElement = screen.getByLabelText("Password");
-  const cpasswordInputElement = screen.getByLabelText(/confirm password/i);
-  expect(emailInputElement.value).toBe("");
-  expect(passwordInputElement.value).toBe("");
-  expect(cpasswordInputElement.value).toBe("");
-});
-
-test("event handler", () => {
-  render(<App />);
+const typeIntoForm = ({ email, password, cpassword }) => {
   const emailInputElement = screen.getByRole("textbox", {
     name: /email/i,
   });
   const passwordInputElement = screen.getByLabelText("Password");
   const cpasswordInputElement = screen.getByLabelText(/confirm password/i);
 
-  userEvent.type(emailInputElement, "abidurrahman471@gmail.com");
-  expect(emailInputElement.value).toBe("abidurrahman471@gmail.com");
+  if (email) {
+    userEvent.type(emailInputElement, email);
+  }
+  if (password) {
+    userEvent.type(passwordInputElement, password);
+  }
+  if (cpassword) {
+    userEvent.type(cpasswordInputElement, cpassword);
+  }
 
-  userEvent.type(cpasswordInputElement, "cpassword!");
-  expect(cpasswordInputElement.value).toBe("cpassword!");
+  return { emailInputElement, passwordInputElement, cpasswordInputElement };
+};
 
-  userEvent.type(passwordInputElement, "password!");
-  expect(passwordInputElement.value).toBe("password!");
-});
-
-test("should show email error message on invalid email", () => {
-  render(<App />);
-  const emailErrorElement = screen.queryByText(
-    /the email you type is invalid/i
-  );
-  const emailInputElement = screen.getByRole("textbox", {
-    name: /email/i,
-  });
-  userEvent.type(emailInputElement, "abidurrahman471gmail.com");
-
-  expect(emailErrorElement).not.toBeInTheDocument();
-
+const btnClickEvent = () => {
   const buttonInputElement = screen.getByRole("button", {
     name: /submit/i,
   });
 
   userEvent.click(buttonInputElement);
-  const emailErrorElementAgain = screen.queryByText(
-    /the email you type is invalid/i
-  );
-  expect(emailErrorElementAgain).toBeInTheDocument();
-});
+};
 
-test("should show password error if password is not strong", () => {
-  render(<App />);
+// Test Function
 
-  const emailInputElement = screen.getByRole("textbox", { name: /email/i });
-  const passwordInputElement = screen.getByLabelText("Password");
-  const passwordErrorElement = screen.queryByText(/password is too weak!/i);
+describe("App", () => {
+  beforeEach(() => {
+    // eslint-disable-next-line testing-library/no-render-in-setup
+    render(<App />);
+  });
 
-  userEvent.type(emailInputElement, "abidurrahman@gmail.com");
-  expect(passwordErrorElement).not.toBeInTheDocument();
+  test("inputs should be in initially empty", () => {
+    const { emailInputElement } = typeIntoForm({
+      email: "",
+    });
+    const { passwordInputElement } = typeIntoForm({ password: "" });
+    const { cpasswordInputElement } = typeIntoForm({
+      cpassword: "",
+    });
 
-  // userEvent.type(passwordInputElement, "@kASH123456789");
-  // expect(passwordErrorElement).not.toBeInTheDocument();
+    expect(emailInputElement.value).toBe("");
+    expect(passwordInputElement.value).toBe("");
+    expect(cpasswordInputElement.value).toBe("");
+  });
 
-  userEvent.type(passwordInputElement, "1234");
+  test("event handler", () => {
+    const { emailInputElement } = typeIntoForm({
+      email: "abidurrahman471@gmail.com",
+    });
+    const { passwordInputElement } = typeIntoForm({
+      password: "@kASH123456789",
+    });
+    const { cpasswordInputElement } = typeIntoForm({
+      cpassword: "@kASH123456789",
+    });
 
-  const buttonInputElement = screen.getByRole("button", { name: /submit/i });
-  userEvent.click(buttonInputElement);
+    expect(emailInputElement.value).toBe("abidurrahman471@gmail.com");
 
-  const passwordErrorElementAgain = screen.queryByText(
-    /password is too weak!/i
-  );
+    expect(cpasswordInputElement.value).toBe("@kASH123456789");
 
-  expect(passwordErrorElementAgain).toBeInTheDocument();
-});
+    expect(passwordInputElement.value).toBe("@kASH123456789");
+  });
 
-test("should show cpassword error if password don't match", () => {
-  render(<App />);
+  test("should show email error message on invalid email", () => {
+    const emailErrorElement = screen.queryByText(
+      /the email you type is invalid/i
+    );
+    // const emailInputElement = screen.getByRole("textbox", {
+    //   name: /email/i,
+    // });
+    // userEvent.type(emailInputElement, "abidurrahman471gmail.com");
 
-  const emailInputElement = screen.getByRole("textbox", { name: /email/i });
-  const passwordInputElement = screen.getByLabelText("Password");
-  const cpasswordInputElement = screen.getByLabelText(/confirm password/i);
+    typeIntoForm({ email: "abidurrahman471gmail.com" });
+    expect(emailErrorElement).not.toBeInTheDocument();
 
-  const cpasswordErrorElement = screen.queryByText(/password don't match!/i);
+    btnClickEvent();
+    const emailErrorElementAgain = screen.queryByText(
+      /the email you type is invalid/i
+    );
+    expect(emailErrorElementAgain).toBeInTheDocument();
+  });
 
-  const submitBtnElement = screen.getByRole("button", { name: /submit/i });
-  userEvent.type(emailInputElement, "abidurrahman471@gmail.com");
-  userEvent.type(passwordInputElement, "@kASH123456789");
+  test("should show password error if password is not strong", () => {
+    typeIntoForm({
+      email: "abidurrahman471@gmail.com",
+    });
 
-  expect(cpasswordErrorElement).not.toBeInTheDocument();
+    expect(
+      screen.queryByText(/password is too weak!/i)
+    ).not.toBeInTheDocument();
 
-  userEvent.type(cpasswordInputElement, "123456");
-  userEvent.click(submitBtnElement);
+    // userEvent.type(passwordInputElement, "@kASH123456789");
 
-  const cpasswordErrorElementAgain = screen.queryByText(
-    /password don't match!/i
-  );
+    btnClickEvent();
+    typeIntoForm({ password: "@kASH" });
+    // eslint-disable-next-line testing-library/prefer-presence-queries
+    expect(screen.queryByText(/password is too weak!/i)).toBeInTheDocument();
+  });
 
-  expect(cpasswordErrorElementAgain).toBeInTheDocument();
-});
+  test("should show cpassword error if password don't match", () => {
+    typeIntoForm({
+      email: "abidurrahman471@gmail.com",
+      password: "@kASH123456789",
+    });
 
-test("should show no error if all input is valid", () => {
-  render(<App />);
+    expect(
+      screen.queryByText(/password don't match!/i)
+    ).not.toBeInTheDocument();
 
-  const emailInputElement = screen.getByRole("textbox", { name: /email/i });
-  const passwordInputElement = screen.getByLabelText("Password");
-  const cpasswordInputElement = screen.getByLabelText(/confirm password/i);
+    // userEvent.type(cpasswordInputElement, "123456");
+    typeIntoForm({ cpassword: "123456" });
+    btnClickEvent();
 
-  const submitBtnElement = screen.getByRole("button", { name: /submit/i });
-  userEvent.type(emailInputElement, "abidurrahman471@gmail.com");
-  userEvent.type(passwordInputElement, "@kASH123456789");
-  userEvent.type(cpasswordInputElement, "@kASH123456789");
+    // eslint-disable-next-line testing-library/prefer-presence-queries
+    expect(screen.queryByText(/password don't match!/i)).toBeInTheDocument();
+  });
 
-  userEvent.click(submitBtnElement);
-  const emailErrorElement = screen.queryByText(
-    /the email you type is invalid/i
-  );
-  const passwordErrorElement = screen.queryByText(/password is too weak!/i);
-  const cpasswordErrorElement = screen.queryByText(/password don't match!/i);
-  expect(emailErrorElement).not.toBeInTheDocument();
-  expect(passwordErrorElement).not.toBeInTheDocument();
-  expect(cpasswordErrorElement).not.toBeInTheDocument();
+  test("should show no error if all input is valid", () => {
+    typeIntoForm({
+      email: "abidurrahman471@gmail.com",
+      password: "@kASH123456789",
+      cpassword: "@kASH123456789",
+    });
+
+    btnClickEvent();
+
+    expect(
+      screen.queryByText(/the email you type is invalid/i)
+    ).not.toBeInTheDocument();
+    expect(
+      screen.queryByText(/password is too weak!/i)
+    ).not.toBeInTheDocument();
+    expect(
+      screen.queryByText(/password don't match!/i)
+    ).not.toBeInTheDocument();
+  });
 });
